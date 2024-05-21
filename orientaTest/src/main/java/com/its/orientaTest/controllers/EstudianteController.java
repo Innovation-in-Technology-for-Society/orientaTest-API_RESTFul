6f7b42fc3eea78ffd305b02a68f38f8c694ef345
Jair Rodriguez <jair1232@hotmail.com>
2024-05-21 17:51:28 -0500

    feat: añadir controller para crear estudiante

--- orientaTest/src/main/java/com/its/orientaTest/controllers/EstudianteController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/EstudianteController.java
@@ -1,5 +1,28 @@
 package com.its.orientaTest.controllers;
 
+
+import com.its.orientaTest.model.dto.EstudianteRequestDTO;
+import com.its.orientaTest.model.dto.EstudianteResponseDTO;
+import com.its.orientaTest.service.EstudianteService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estudiantes")
+@AllArgsConstructor
 public class EstudianteController {
-    
+
+    private final EstudianteService estudianteService;
+
+    @PostMapping
+    public ResponseEntity<EstudianteResponseDTO> createEstudiante(@Validated @RequestBody EstudianteRequestDTO estudianteRequestDTO){
+        EstudianteResponseDTO createdEstudiante = estudianteService.createEstudiante(estudianteRequestDTO);
+        return new ResponseEntity<>(createdEstudiante, HttpStatus.CREATED);
+    }
 }
