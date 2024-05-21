551f041b20155033bdcc6d7afe45201d39d895da
Mauro <mauroadauto9@gmail.com>
2024-05-20 22:10:57 -0500

    dumb comimit: controllers/

---
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/ResultadoController.java
@@ -0,0 +1,18 @@
+package com.its.orientaTest.controllers;
+
+import com.its.orientaTest.model.dto.ResultadoResponseDTO;
+import com.its.orientaTest.service.ResultadoService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resultados")
+@AllArgsConstructor
+public class ResultadoController {
+
+}
