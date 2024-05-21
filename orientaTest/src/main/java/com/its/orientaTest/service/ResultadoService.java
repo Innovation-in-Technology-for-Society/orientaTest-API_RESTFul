5832f97be8d9adb42cb45cb85f49bf28bcb60074
Mauro <mauroadauto9@gmail.com>
2024-05-20 21:59:52 -0500

    feat: añadir Ver todos los Resultados

--- orientaTest/src/main/java/com/its/orientaTest/service/ResultadoService.java
+++ orientaTest/src/main/java/com/its/orientaTest/service/ResultadoService.java
@@ -1,5 +1,26 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.exceptions.ResourceNotFoundException;
+import com.its.orientaTest.mapper.ResultadoMapper;
+import com.its.orientaTest.model.dto.ResultadoResponseDTO;
+import com.its.orientaTest.model.entities.Resultado;
+import com.its.orientaTest.repository.ResultadoRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class ResultadoService {
-    
+    private final ResultadoRepository resultadoRepository;
+    private final ResultadoMapper resultadoMapper;
+
+    @Transactional(readOnly = true)
+    public List<ResultadoResponseDTO> getAllResultados(){
+        List<Resultado> resultados = resultadoRepository.findAll();
+        return resultadoMapper.toListDTO(resultados);
+    }
 }
