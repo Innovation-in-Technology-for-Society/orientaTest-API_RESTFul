e5961d122713c24d7fc943dd4da595b954bbbca0
Jair Rodriguez <jair1232@hotmail.com>
2024-05-21 18:22:56 -0500

    feat: añadir controller de crear universidad

--- orientaTest/src/main/java/com/its/orientaTest/controllers/UniversidadController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/UniversidadController.java
@@ -1,5 +1,26 @@
 package com.its.orientaTest.controllers;
 
+import com.its.orientaTest.model.dto.UniversidadRequestDTO;
+import com.its.orientaTest.model.dto.UniversidadResponseDTO;
+import com.its.orientaTest.service.UniversidadService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/universidades")
+@AllArgsConstructor
 public class UniversidadController {
+    private final UniversidadService universidadService;
     
+    @PostMapping
+    public ResponseEntity<UniversidadResponseDTO> createUniversidad(@Validated @RequestBody UniversidadRequestDTO universidadRequestDTO){
+        UniversidadResponseDTO createdUniversidad = universidadService.createUniversidad(universidadRequestDTO);
+        return new ResponseEntity<>(createdUniversidad, HttpStatus.CREATED);
+    }
 }
