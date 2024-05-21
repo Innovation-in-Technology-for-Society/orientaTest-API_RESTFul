64ff972bb3daad8dfbcf8053e657a226a4d9caa1
Jair Rodriguez <jair1232@hotmail.com>
2024-05-21 18:07:05 -0500

    feat: añadir crear carrera

--- orientaTest/src/main/java/com/its/orientaTest/service/CarreraService.java
+++ orientaTest/src/main/java/com/its/orientaTest/service/CarreraService.java
@@ -1,5 +1,34 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.exceptions.ResourceDuplicateException;
+import com.its.orientaTest.exceptions.ResourceNotFoundException;
+import com.its.orientaTest.mapper.CarreraMapper;
+import com.its.orientaTest.model.dto.CarreraRequestDTO;
+import com.its.orientaTest.model.dto.CarreraResponseDTO;
+import com.its.orientaTest.model.entities.Carrera;
+import com.its.orientaTest.repository.CarreraRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class CarreraService {
-    
+    private final CarreraRepository carreraRepository;
+    private final CarreraMapper carreraMapper;
+
+    @Transactional
+    public CarreraResponseDTO createCarrera(CarreraRequestDTO carreraRequestDTO){
+        // Verificar si la Carrera existe
+        if (carreraRepository.findByNombre(carreraRequestDTO.getNombre()).isPresent()){
+            throw new ResourceDuplicateException("La carrera ya existe");
+        }
+
+        Carrera carrera = carreraMapper.toEntity(carreraRequestDTO);
+        carreraRepository.save(carrera);
+        return carreraMapper.toDTO(carrera);
+    }
 }
