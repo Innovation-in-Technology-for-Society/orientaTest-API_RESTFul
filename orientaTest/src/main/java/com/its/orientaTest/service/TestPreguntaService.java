8609973979540d3a26afd404650039ecf08eb673
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-22 18:36:21 -0500

    docs: añadir TestPreguntaService

---
+++ orientaTest/src/main/java/com/its/orientaTest/service/TestPreguntaService.java
@@ -0,0 +1,21 @@
+package com.its.orientaTest.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.its.orientaTest.model.dto.PreguntaResponseDTO;
+import com.its.orientaTest.model.dto.TestPreguntaResponseDTO;
+import com.its.orientaTest.repository.TestPreguntaRepository;
+import com.its.orientaTest.model.entities.Pregunta;
+import com.its.orientaTest.model.entities.TestPregunta;
+
+import org.springframework.transaction.annotation.Transactional;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class TestPreguntaService {
+    
+}
