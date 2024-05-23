6a405034be8763414510e6926bb979326d81b078
marck0820001 <marck-08-2001@hotmail.com>
2024-05-22 23:56:27 -0500

    feat: añadir encontrar pago por estudiante id

--- orientaTest/src/main/java/com/its/orientaTest/repository/PagoRepository.java
+++ orientaTest/src/main/java/com/its/orientaTest/repository/PagoRepository.java
@@ -2,9 +2,14 @@ package com.its.orientaTest.repository;
 
 import com.its.orientaTest.model.entities.Pago;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PagoRepository extends JpaRepository<Pago, Long> {
-
+    @Query("SELECT p FROM Pago p WHERE p.estudiante.id = :idEstudiante")
+    List<Pago> findByIdEstudiante(@Param("idEstudiante") Long idEstudiante);
 }
