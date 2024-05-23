4827c5726b560a0111363e4a82510c888f2028b1
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-22 20:16:20 -0500

    feat: añadir mapper toDTOPrecisa

--- orientaTest/src/main/java/com/its/orientaTest/mapper/UniversidadMapper.java
+++ orientaTest/src/main/java/com/its/orientaTest/mapper/UniversidadMapper.java
@@ -2,6 +2,7 @@ package com.its.orientaTest.mapper;
 
 import com.its.orientaTest.model.dto.UniversidadRequestDTO;
 import com.its.orientaTest.model.dto.UniversidadResponseDTO;
+import com.its.orientaTest.model.dto.UniversidadPrecisaResponseDTO;
 import com.its.orientaTest.model.entities.Universidad;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -25,4 +26,10 @@ public class UniversidadMapper {
     public List<UniversidadResponseDTO> toListDTO(List<Universidad> universidades){
         return universidades.stream().map(this::toDTO).toList();
     }
+
+    public UniversidadPrecisaResponseDTO toDTOPrecisa(Universidad universidad){
+        return modelMapper.map(universidad, UniversidadPrecisaResponseDTO.class);
+    }
+
+    
 }
