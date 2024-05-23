aa873e62d9becd2f9c833253768f9d0755ac18e3
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-22 20:29:54 -0500

    feat: añadir obtener universidad precisa

--- orientaTest/src/main/java/com/its/orientaTest/service/UniversidadService.java
+++ orientaTest/src/main/java/com/its/orientaTest/service/UniversidadService.java
@@ -1,11 +1,25 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.mapper.UniversidadMapper;
+import com.its.orientaTest.model.entities.Universidad;
+import com.its.orientaTest.model.dto.UniversidadPrecisaResponseDTO;
+import com.its.orientaTest.repository.UniversidadRepository;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class UniversidadService {
-    
+    private final UniversidadRepository universidadRepository;
+    private final UniversidadMapper universidadMapper;
+
+    @Transactional(readOnly = true)
+    public List<UniversidadPrecisaResponseDTO> getUniversidadPrecisa(){
+        List<Universidad> universidad = universidadRepository.findAll();
+        return universidadMapper.toListDTOPrecisa((universidad));
+    }
 }
