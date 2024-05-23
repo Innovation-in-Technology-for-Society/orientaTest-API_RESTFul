3adb95d415064355822a3b3140f27ba157d0203f
marck0820001 <marck-08-2001@hotmail.com>
2024-05-23 00:07:43 -0500

    feat: añadir controller obtener beneficio de pago por estudiante id

--- orientaTest/src/main/java/com/its/orientaTest/controller/PagoController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controller/PagoController.java
@@ -1,5 +1,25 @@
 package com.its.orientaTest.controller;
 
+import com.its.orientaTest.model.dto.PagoBeneficioResponseDTO;
+import com.its.orientaTest.service.PagoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/pagos")
+@AllArgsConstructor
 public class PagoController {
-    
+    private final PagoService pagoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<PagoBeneficioResponseDTO>> getBeneficioById(@PathVariable Long id){
+        List<PagoBeneficioResponseDTO> pagoBeneficioResponseDTO = pagoService.getPagosBeneficioByEstudianteId(id);
+        return new ResponseEntity<>(pagoBeneficioResponseDTO, HttpStatus.OK);
+    }
 }
