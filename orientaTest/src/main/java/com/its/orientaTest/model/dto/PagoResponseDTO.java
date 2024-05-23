2147c5d7230fbf1338782940c41e63e71e0f4266
marck0820001 <marck-08-2001@hotmail.com>
2024-05-22 23:45:04 -0500

    feat: añadir PagoResponseDTO

---
+++ orientaTest/src/main/java/com/its/orientaTest/model/dto/PagoResponseDTO.java
@@ -0,0 +1,14 @@
+package com.its.orientaTest.model.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagoResponseDTO {
+    private Long idPago;
+    private String nombreEstudiante;
+    private BigDecimal monto;
+    private String beneficio;
+}
