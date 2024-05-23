58e9f65d277c294fa5568bdd0bab8d4aa07c4255
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-22 20:29:24 -0500

    docs: añadir UniversidadService

---
+++ orientaTest/src/main/java/com/its/orientaTest/service/UniversidadService.java
@@ -0,0 +1,11 @@
+package com.its.orientaTest.service;
+
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+
+
+@Service
+@AllArgsConstructor
+public class UniversidadService {
+    
+}
