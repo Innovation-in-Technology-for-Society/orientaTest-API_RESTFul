d59196db2397badb21a946fc3c8d52f320ee5cd7
marck0820001 <marck-08-2001@hotmail.com>
2024-05-22 23:40:19 -0500

    feat: actualizar entity Pago

--- orientaTest/src/main/java/com/its/orientaTest/model/entities/Pago.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/entities/Pago.java
@@ -1,5 +1,34 @@
 package com.its.orientaTest.model.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pago")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pago {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "monto", nullable = false)
+    private BigDecimal monto;
+
+    @Column(name = "fecha_pago", nullable = false)
+    private LocalDate fechaPagon;
+
+    @Column(name = "beneficio", nullable = false)
+    private String beneficio;
+
+    @ManyToOne
+    @JoinColumn(name = "id_estudiante", nullable = false)
+    private Estudiante estudiante;
+
 }
