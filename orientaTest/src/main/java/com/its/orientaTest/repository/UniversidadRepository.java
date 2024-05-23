3c7d3eda97a57aed501d024ebcdad3790fde8f85
marck0820001 <marck-08-2001@hotmail.com>
2024-05-23 01:31:18 -0500

    fix: eliminar  UniversidadRepository

--- orientaTest/src/main/java/com/its/orientaTest/repository/UniversidadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.its.orientaTest.repository;
-
-import com.its.orientaTest.model.entities.Universidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UniversidadRepository extends JpaRepository<Universidad, Long> {
-    
-}
