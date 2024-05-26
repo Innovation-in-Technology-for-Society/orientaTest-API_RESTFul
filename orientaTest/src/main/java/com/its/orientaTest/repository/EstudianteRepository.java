101d92f1ab2a0ea08b0fbe37b6516cf4193faa4a
David Niño <daavidninoosuaarez@gmail.com>
2024-05-25 22:19:32 -0500

    feat: añadir heredar metodos de operaciones CRUD

--- orientaTest/src/main/java/com/its/orientaTest/repository/EstudianteRepository.java
+++ orientaTest/src/main/java/com/its/orientaTest/repository/EstudianteRepository.java
@@ -1,5 +1,8 @@
 package com.its.orientaTest.repository;
 
-public class EstudianteRepository {
-    
+import com.its.orientaTest.model.entities.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long>{
+
 }
