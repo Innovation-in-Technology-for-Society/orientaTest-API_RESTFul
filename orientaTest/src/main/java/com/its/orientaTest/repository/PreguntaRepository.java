6eedd8e64d017b694319e32a3eef4c8d391e6f01
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 09:20:48 -0500

    feat: añadir obtener preguntas de vocacion

--- orientaTest/src/main/java/com/its/orientaTest/repository/PreguntaRepository.java
+++ orientaTest/src/main/java/com/its/orientaTest/repository/PreguntaRepository.java
@@ -1,5 +1,14 @@
 package com.its.orientaTest.repository;
 
-public class PreguntaRepository {
-    
+import com.its.orientaTest.model.entities.Pregunta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PreguntaRepository extends JpaRepository<Pregunta, Long>{
+    // Find Vocacional By categoria 1-3
+    @Query(value = "SELECT p FROM Pregunta p WHERE p.categoria.id IN (1, 2, 3)")
+    List<Pregunta> findVocacional();
+
 }
