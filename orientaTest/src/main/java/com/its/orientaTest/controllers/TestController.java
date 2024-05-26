c18bda8bb27ecd301e4c47b65e783c1d376a7cb2
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 17:29:27 -0500

    feat: añadir controller iniciar test

--- orientaTest/src/main/java/com/its/orientaTest/controllers/TestController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/TestController.java
@@ -1,5 +1,24 @@
 package com.its.orientaTest.controllers;
 
+import com.its.orientaTest.model.dto.TestRequestDTO;
+import com.its.orientaTest.model.dto.TestResponseDTO;
+import com.its.orientaTest.service.TestService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tests")
+@AllArgsConstructor
 public class TestController {
-    
+    private final TestService testService;
+
+    @PostMapping
+    public ResponseEntity<TestResponseDTO> startTest(@Validated @RequestBody TestRequestDTO testRequestDTO){
+        TestResponseDTO takenTest = testService.startTest(testRequestDTO);
+        return new ResponseEntity<>(takenTest, HttpStatus.CREATED);
+    }
 }
