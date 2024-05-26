480a94e287301e19ab0ebafd8f149c96dfd9602e
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-26 17:57:55 -0500

    feat: añadir controller obtener test por estudiante id

--- orientaTest/src/main/java/com/its/orientaTest/controller/TestController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.its.orientaTest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.its.orientaTest.model.dto.TestResponseDTO;
+import com.its.orientaTest.service.TestService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,5 +16,11 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/tests")
 @AllArgsConstructor
 public class TestController {
+    private final TestService testService;
     
+    @GetMapping("/{estudiante_id}")
+    public ResponseEntity<TestResponseDTO> getTestByEstudianteId(@PathVariable Long estudiante_id){
+        TestResponseDTO test = testService.getTestByEstudianteId(estudiante_id);
+        return new ResponseEntity<>(test, HttpStatus.OK);
+    }    
 }
