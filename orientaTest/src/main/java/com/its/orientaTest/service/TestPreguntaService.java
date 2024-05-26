a610cf2f47708f3e3bc61820a0fbf21d7b52ae03
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 17:25:22 -0500

    feat: añadir obtener preguntas de vocacion

--- orientaTest/src/main/java/com/its/orientaTest/service/TestPreguntaService.java
+++ orientaTest/src/main/java/com/its/orientaTest/service/TestPreguntaService.java
@@ -1,23 +1,39 @@
 package com.its.orientaTest.service;
 
 import com.its.orientaTest.exceptions.ResourceNotFoundException;
+import com.its.orientaTest.mapper.TestPreguntaMapper;
+import com.its.orientaTest.model.dto.TestPreguntaRequestDTO;
+import com.its.orientaTest.model.dto.TestPreguntaResponseDTO;
 import com.its.orientaTest.model.entities.Pregunta;
 import com.its.orientaTest.model.entities.Test;
 import com.its.orientaTest.model.entities.TestPregunta;
+import com.its.orientaTest.repository.PreguntaRepository;
 import com.its.orientaTest.repository.TestPreguntaRepository;
 import com.its.orientaTest.repository.TestRepository;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
+
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TestPreguntaService {
+    private final TestPreguntaMapper testPreguntaMapper;
     private final TestRepository testRepository;
+    private final PreguntaRepository preguntaRepository;
     private final TestPreguntaRepository testPreguntaRepository;
 
+    @Transactional 
+    public void getPreguntasVocacionales(Long test_id){
+        List<Pregunta> preguntas = preguntaRepository.findVocacional();
+        Collections.shuffle(preguntas);
+        preguntas = preguntas.subList(0, Math.min(preguntas.size(), 25));
+        savePreguntas(test_id,  preguntas, "vocacional");
+    }
+    
     @Transactional
     public void savePreguntas(Long test_id, List<Pregunta> preguntas, String tipo_test){
         for (Pregunta pregunta : preguntas){
