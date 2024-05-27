37c8aba2fb147e1c6a07d3164d9782af6d0ada17
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 21:05:03 -0500

    feat: añadir encontrar carrera por nombre

--- orientaTest/src/main/java/com/its/orientaTest/repository/CarreraRepository.java
+++ orientaTest/src/main/java/com/its/orientaTest/repository/CarreraRepository.java
@@ -1,5 +1,11 @@
 package com.its.orientaTest.repository;
 
-public class CarreraRepository {
-    
+import com.its.orientaTest.model.entities.Carrera;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CarreraRepository extends JpaRepository<Carrera, Long>{
+    // Find Carrera By Name
+    Optional<Carrera> findByNombre(String nombre);
 }
