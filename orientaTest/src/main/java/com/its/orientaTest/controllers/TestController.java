7b77f71a0ba08c37df5f94829bcd37ea5037a660
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 22:23:45 -0500

    feat: añadir controller generar resultado

--- orientaTest/src/main/java/com/its/orientaTest/controllers/TestController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/TestController.java
@@ -1,5 +1,22 @@
 package com.its.orientaTest.controllers;
 
+import com.its.orientaTest.model.dto.TestResponseDTO;
+import com.its.orientaTest.service.TestService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tests")
+@AllArgsConstructor
 public class TestController {
-    
+    private final TestService testService;
+
+    @PostMapping("{test_id}/resultado")
+    public ResponseEntity<TestResponseDTO> generateResultado(@PathVariable Long test_id){
+        TestResponseDTO finishedTest = testService.generateResultado(test_id);
+        return new ResponseEntity<>(finishedTest, HttpStatus.CREATED);
+    }
 }
