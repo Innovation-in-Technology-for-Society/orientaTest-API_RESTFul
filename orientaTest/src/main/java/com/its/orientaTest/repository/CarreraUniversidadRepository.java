d0364702255854a61ece40d6d972a2e0ec63fa86
David Niño <daavidninoosuaarez@gmail.com>
2024-05-26 21:56:24 -0500

    feat: añadir encontrar CarreraUniversidad por universidad y carrera

--- orientaTest/src/main/java/com/its/orientaTest/repository/CarreraUniversidadRepository.java
+++ orientaTest/src/main/java/com/its/orientaTest/repository/CarreraUniversidadRepository.java
@@ -1,5 +1,13 @@
 package com.its.orientaTest.repository;
 
-public class CarreraUniversidadRepository {
-    
+import com.its.orientaTest.model.entities.CarreraUniversidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CarreraUniversidadRepository extends JpaRepository<CarreraUniversidad, Long>{
+    //Carrera y Universidad 
+    @Query("SELECT cu FROM CarreraUniversidad cu WHERE cu.universidad.id = :universidad_id AND cu.carrera.id = :carrera_id")
+    Optional<CarreraUniversidad> findByUniversidadAndCarrera(Long universidad_id, Long carrera_id);
 }
