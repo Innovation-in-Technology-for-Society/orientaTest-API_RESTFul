7f696b1df61a366c035175656704004dabf69d60
Mauro <mauroadauto9@gmail.com>
2024-05-28 18:43:24 -0500

    feat: getUniversidadByNombre en UniversidadController

--- orientaTest/src/main/java/com/its/orientaTest/controllers/UniversidadController.java
+++ orientaTest/src/main/java/com/its/orientaTest/controllers/UniversidadController.java
@@ -1,5 +1,23 @@
 package com.its.orientaTest.controllers;
 
+import com.its.orientaTest.model.dto.UniversidadResponseDTO;
+import com.its.orientaTest.service.UniversidadService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/universidades")
+@AllArgsConstructor
 public class UniversidadController {
-    
+    private final UniversidadService universidadService;
+
+    @GetMapping("/{nombre}")
+    public ResponseEntity<UniversidadResponseDTO> getUniversidadByNombre(@PathVariable String nombre){
+        UniversidadResponseDTO universidad = universidadService.getUniversidadByNombre(nombre);
+        return new ResponseEntity<>(universidad, HttpStatus.OK);
+    }
 }
