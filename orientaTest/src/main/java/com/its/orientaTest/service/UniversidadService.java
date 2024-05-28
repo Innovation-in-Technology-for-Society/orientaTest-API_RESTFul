b0a0c7fed88db49627a2ada9475e1b806c40e034
Mauro <mauroadauto9@gmail.com>
2024-05-28 18:42:09 -0500

    feat: getUniversidadByNombre en UniversidadService

--- orientaTest/src/main/java/com/its/orientaTest/service/UniversidadService.java
+++ orientaTest/src/main/java/com/its/orientaTest/service/UniversidadService.java
@@ -1,5 +1,25 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.exceptions.ResourceNotFoundException;
+import com.its.orientaTest.mapper.UniversidadMapper;
+import com.its.orientaTest.model.entities.Universidad;
+import com.its.orientaTest.model.dto.UniversidadResponseDTO;
+import com.its.orientaTest.repository.UniversidadRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class UniversidadService {
-    
+    private final UniversidadRepository universidadRepository;
+    private final UniversidadMapper universidadMapper;
+
+    @Transactional(readOnly = true)
+    public UniversidadResponseDTO getUniversidadByNombre(String nombre){
+        Universidad universidad = universidadRepository.findByNombre(nombre)
+        .orElseThrow(() -> new ResourceNotFoundException("Universidad no encontrada con nombre: " + nombre));
+        return universidadMapper.toDTO(universidad);
+    }
 }
