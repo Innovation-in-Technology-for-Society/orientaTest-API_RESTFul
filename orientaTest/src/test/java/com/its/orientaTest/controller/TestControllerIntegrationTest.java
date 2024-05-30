fcf82e33e07bdac08d0f2710c65138cde9e36fca
David Niño <daavidninoosuaarez@gmail.com>
2024-05-30 00:15:07 -0500

    test: añadir test de integracion de generar resultado

--- orientaTest/src/test/java/com/its/orientaTest/controller/TestControllerIntegrationTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/controller/TestControllerIntegrationTest.java
@@ -1,5 +1,26 @@
 package com.its.orientaTest.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestControllerIntegrationTest {
-    
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGenerateResultado() throws Exception {
+        // Simular una solicitud de creación de resultado de prueba con un ID de prueba
+        mockMvc.perform(MockMvcRequestBuilders.post("/tests/1/resultado")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
 }
