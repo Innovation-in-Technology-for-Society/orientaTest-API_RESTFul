ad886744db7232e292190d7620fad5478f3e90ca
Groppoxx <iam_alvarez_orellana@hotmail.com>
2024-05-30 01:03:37 -0500

    test: prueba de integracion testGetTestByEstudianteIdTest

--- orientaTest/src/test/java/com/its/orientaTest/controller/TestControllerIntegrationTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/controller/TestControllerIntegrationTest.java
@@ -1,5 +1,22 @@
 package com.its.orientaTest.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestControllerIntegrationTest {
-    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetTestByEstudianteIdTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/tests/{estudiante_id}", 1L))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
