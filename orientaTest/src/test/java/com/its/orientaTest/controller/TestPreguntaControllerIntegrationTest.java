b7e184050c1425ffdeac501c5a3be45e68dc5457
Marco Antonio F.R.O <84866607+marck0820001@users.noreply.github.com>
2024-05-30 18:27:50 -0500

    Revert "Test/Visualización de resultados detallados del test vocacional"

--- orientaTest/src/test/java/com/its/orientaTest/controller/TestPreguntaControllerIntegrationTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/controller/TestPreguntaControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.its.orientaTest.model.dto.TestPreguntaRequestDTO;
 import com.its.orientaTest.model.dto.TestPreguntaResponseDTO;
 import com.its.orientaTest.service.TestPreguntaService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,8 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -58,17 +57,5 @@ public class TestPreguntaControllerIntegrationTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-  
-    @Test
-    public void testGetResultadosTipoTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/test-preguntas/{test_id}/{tipoTest}", 1L, "auto-percepcion"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-  
-    @Test
-    public void testGetResultadosTipoTestVocacional() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/test-preguntas/{test_id}/{tipoTest}", 1L, "vocacional"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
