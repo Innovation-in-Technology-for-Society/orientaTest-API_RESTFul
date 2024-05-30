92299ce159a65b69ba37018b8a279b026f0cfcb4
marck0820001 <marck-08-2001@hotmail.com>
2024-05-30 02:18:12 -0500

    test: prueba de integracion testGetResultadosTipoTest

--- orientaTest/src/test/java/com/its/orientaTest/controller/TestPreguntaControllerIntegrationTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/controller/TestPreguntaControllerIntegrationTest.java
@@ -1,5 +1,23 @@
 package com.its.orientaTest.controller;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestPreguntaControllerIntegrationTest {
-    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetResultadosTipoTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test-preguntas/{test_id}/{tipoTest}", 1L, "vocacional"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
