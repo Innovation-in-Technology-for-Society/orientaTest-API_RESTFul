21a13aaf6f665f462182a8214010903642d2625d
marck0820001 <marck-08-2001@hotmail.com>
2024-05-30 02:07:00 -0500

    test: prueba unitaria testGetPagosBeneficioByEstudianteId_EstudianteNoEncontrado caso no exitoso

--- orientaTest/src/test/java/com/its/orientaTest/service/PagoServiceTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/service/PagoServiceTest.java
@@ -1,5 +1,49 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.exceptions.ResourceNotFoundException;
+import com.its.orientaTest.mapper.PagoMapper;
+import com.its.orientaTest.model.dto.PagoBeneficioResponseDTO;
+import com.its.orientaTest.model.entities.Pago;
+import com.its.orientaTest.repository.EstudianteRepository;
+import com.its.orientaTest.repository.PagoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
 public class PagoServiceTest {
-    
+    @Mock
+    private PagoMapper pagoMapper;
+
+    @Mock
+    private PagoRepository pagoRepository;
+
+    @Mock
+    private EstudianteRepository estudianteRepository;
+
+    @InjectMocks
+    private PagoService pagoService;
+
+    @Test
+    public void testGetPagosBeneficioByEstudianteId_EstudianteNoEncontrado() {
+        Long idEstudiante = 3L;
+
+        when(estudianteRepository.existsById(idEstudiante)).thenReturn(false);
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
+            pagoService.getPagosBeneficioByEstudianteId(idEstudiante);
+        });
+
+        assertEquals("Estudiante no encontrado", exception.getMessage());
+    }    
 }
