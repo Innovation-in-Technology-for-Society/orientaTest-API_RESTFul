e793801b5a104ff3d820466818ae28bd7e13e98b
marck0820001 <marck-08-2001@hotmail.com>
2024-05-30 02:16:14 -0500

    test: prueba unitaria testGetResultadosTipoTest_ReturnsEmptyList caso exitoso

--- orientaTest/src/test/java/com/its/orientaTest/service/TestPreguntaServiceTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/service/TestPreguntaServiceTest.java
@@ -1,5 +1,45 @@
 package com.its.orientaTest.service;
 
+import com.its.orientaTest.mapper.TestPreguntaMapper;
+import com.its.orientaTest.model.dto.PreguntaResponseDTO;
+import com.its.orientaTest.model.dto.TestPreguntaResponseDTO;
+import com.its.orientaTest.model.entities.Pregunta;
+import com.its.orientaTest.model.entities.TestPregunta;
+import com.its.orientaTest.repository.TestPreguntaRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TestPreguntaServiceTest {
-    
+    @Mock
+    private TestPreguntaRepository testPreguntaRepository;
+
+    @Mock
+    private TestPreguntaMapper testPreguntaMapper;
+
+    @InjectMocks
+    private TestPreguntaService testPreguntaService;
+
+    @Test
+    public void testGetResultadosTipoTest_ReturnsEmptyList() {
+        Long test_id = 1L;
+        String tipoTest = "auto-percepcion";
+
+        when(testPreguntaRepository.findByTestIdAndTipoTest(test_id, tipoTest)).thenReturn(Collections.emptyList());
+
+        List<TestPreguntaResponseDTO> result = testPreguntaService.getResultadosTipoTest(test_id, tipoTest);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
 }
