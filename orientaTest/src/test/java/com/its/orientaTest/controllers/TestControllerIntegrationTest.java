cd0f5be100b96e06a232945563573e8bc9203609
David Niño <daavidninoosuaarez@gmail.com>
2024-05-30 19:10:46 -0500

    fix commit

--- orientaTest/src/test/java/com/its/orientaTest/controllers/TestControllerIntegrationTest.java
+++ orientaTest/src/test/java/com/its/orientaTest/controllers/TestControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.its.orientaTest.controller;
+package com.its.orientaTest.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
